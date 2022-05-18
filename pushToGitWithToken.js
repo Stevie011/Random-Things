fc0fced795d99a4fbf1bc2261e248c52043ca902
Stevie011 <60195287+Stevie011@users.noreply.github.com>
2022-05-18 15:31:02 +0200

    Create pushToGitWithToken.js

---
+++ pushToGitWithToken.js
@@ -0,0 +1,4 @@
+//this website https://techglimpse.com/git-push-github-token-based-passwordless/
+
+this line  here:
+  git push https://<GITHUB_ACCESS_TOKEN>@github.com/<GITHUB_USERNAME>/<REPOSITORY_NAME>.git
